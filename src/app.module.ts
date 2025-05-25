335e6df1e2630bf865c739ecb7003fd8b3a07f7d
Saurav <saurav65069@gmail.com>
2025-05-25 23:55:01 +0800

    mongoose, class validators and jwt library added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-chat'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      subscriptions: {
+        'graphql-ws': true,
+        'subscriptions-transport-ws': true,
+      },
+      context: ({ req, connection }) => {
+        if (connection) {
+          return { req: connection.context };
+        }
+        return { req };
+      },
+    }),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'nestjs-chat-secret-key',
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
 })
 export class AppModule {}
